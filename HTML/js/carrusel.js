3b8988d763d360b862c47902b1a86d5bd5401f7b
David Leonardo Gamboa López <84582710+DaveQuake@users.noreply.github.com>
2023-09-18 17:47:16 -0500

    Update carrusel.js

---
+++ HTML/js/carrusel.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    var swiper = new Swiper('.swiper-container', {
+        slidesPerView: 'auto',
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+});
